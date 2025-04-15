392e9d82cc7c01ae0c0c1f3cc854765f6b4bd33e
Ryan <ryewood@gmail.com>
2025-04-15 16:18:27 -0400

    afternoon push

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 
 import './App.css';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import CurrencyRow from './CurrencyRow';
+
 const BASE_URL = 'https://api.frankfurter.app/latest?from=EUR';
 function App() {
-  const [currencyData, setCurrencyData] = React.useState({});
 
+  const [currencyOptions, setCurrencyOptions] = useState([]);
+  const [currencyData, setCurrencyData] = React.useState({});
+  const [toCurrency, setToCurrency] = React.useState();
+  const [exchangeRate, setExchangeRate] = React.useState();
+  const [fromCurrency, setFromCurrency] = React.useState();
+  const [amount, setAmount] = React.useState(1);
+  const [amountInFromCurrency, setAmountInFromCurrency] = React.useState(true);
+
+
+
+  let toAmount, fromAmount;
+  if (amountInFromCurrency) {
+    fromAmount = amount;
+    toAmount = amount * exchangeRate;
+  } else {
+    toAmount = amount;
+    fromAmount = amount / exchangeRate;
+  }
+ 
   useEffect(() => {
     fetch(BASE_URL)
       .then(res => res.json())
       .then(data => {
-        setCurrencyData(data);
+        const firstCurrency = Object.keys(data.rates)[0]
+        const options = [data.base, ...Object.keys(data.rates)];
+        setCurrencyOptions(options);
+        setCurrencyData(options);
+        setFromCurrency(data.base);
+        setToCurrency(firstCurrency);
+        setExchangeRate(data.rates[firstCurrency])
       })
       .catch(error => console.error('Error fetching currency data:', error));
   }, []);
@@ -22,15 +47,36 @@ function App() {
       });
   }, []);
   
+  function handleFromAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(true);
+  }
 
-  return <>
-  
- <h1> Hello World</h1>
- <h2> I am creating a react currency converter </h2>
- <CurrencyRow />
- <div> = </div>
- <CurrencyRow />
-  </>
+  function handleToAmountChange(e) {
+    setAmount(e.target.value)
+    setAmountInFromCurrency(false);
+  }
+return (
+  <>
+    <h1>Hello World</h1>
+    <h2>I am creating a react currency converter</h2>
+    <CurrencyRow 
+    currencyOptions={currencyOptions}
+    selectCurrency = {fromCurrency}
+    onChangeCurrency = {e => setFromCurrency(e.target.value)}
+  onChangeAmount={handleFromAmountChange}
+    amount={fromAmount}
+    />
+    <div>=</div>
+    <CurrencyRow
+        currencyOptions={currencyOptions}
+            selectCurrency = {toCurrency}
+            onChangeCurrency = {e => setToCurrency(e.target.value)}
+            onChangeAmount={handleToAmountChange}
+                  amount={toAmount} />
+
+   </>
+);
 }
 
 export default App;
